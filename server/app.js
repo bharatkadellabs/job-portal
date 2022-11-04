070c453fb78934035d338ef466c3847eff77e662
bharatkadellabs <106664708+bharatkadellabs@users.noreply.github.com>
2022-11-04 17:02:57 +0530

    add backend on firebase

--- server/app.js
+++ server/app.js
@@ -8,11 +8,10 @@ const url = process.env.DB_URL
 mongoose.connect(url).then((res) => console.log("connect Data base"));
 const port = 8000;
 const path = require('path')
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 //To allow cross-origin requests
 app.use(cors());
 app.use("/", indexRouter);
-app.listen(port, (res) => console.log(`running on ports ${port}`, res))
+app.listen(port, () => console.log(`running on ports ${port}`))
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Outlet } from 'react-router-dom'
 import { Router as Routes } from './Router/router'
 import axios from 'axios';
 function App() {
-  axios.defaults.baseURL = window.origin.includes('localhost') ? 'http://localhost:8000' : 'https://bb-job-portal.herokuapp.com/';
+  axios.defaults.baseURL = window.origin.includes('localhost') ? 'http://localhost:8000' : 'https://job-portal-7072c.web.app';
 
   return (
     <BrowserRouter >
